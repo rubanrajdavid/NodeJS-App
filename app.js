818dda07bfda769cab7c0a5b128d744c7bbc413b
Ruban Raj David <rajruban.d@gmail.com>
2020-07-14 21:43:02 +0530

    Added Route Folder

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
-const mysql_lib = require("mysql");
 const bodyParser = require("body-parser");
-
-
-
+const db = require("./src/model/sqlconnection");
 
 //Create an object in Express
 const app = express();
-const appRoutes = require('./routes')(app)
-//Assign the View folder as static address
-app.use(express.static("./view"));
-
-//Log each request URI ,Status and its response time
-app.use(morgan("dev"));
 
 //Retiving data from req sent as JSON in body
 app.use(
@@ -23,6 +14,24 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
+
+//Routes
+app.use('/', require('./src/routes/general'))
+app.use('/user', require('./src/routes/user'))
+
+//Test DB Connection
+db.authenticate()
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((error) => console.log(error));
+
+//Assign the View folder as static address
+app.use(express.static("./view"));
+
+//Log each request URI ,Status and its response time
+app.use(morgan("dev"));
+
 
 //Start Express Server
 app.listen(3002, () => {
