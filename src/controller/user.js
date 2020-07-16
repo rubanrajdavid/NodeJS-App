9ee8d48965c0993b26f72b62f9e2f7ea0c5f092a
Ruban Raj David <rajruban.d@gmail.com>
2020-07-16 21:29:04 +0530

    Added Verify Email function and Verification Link Validation

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,4 +1,5 @@
 const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 const {
     User,
     new_user
@@ -104,15 +105,64 @@ let controller = {
             console.error(error)
         })
     },
-    update_unverified_new_user: (email_id, name) => {
+    update_unverified_new_user: (email_id, name, user_otp) => {
         new_user.update({
             name: name,
-            otp: controller.generateOTP(15)
+            otp: user_otp
         }, {
             where: {
                 mail: email_id
             }
-        })
+        });
+        return user_otp
+    },
+    send_verification_mail: async (email_id, name, user_otp) => {
+        let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: "",
+                pass: "",
+            },
+        });
+        let info = await transporter.sendMail({
+            from: '" 👻" <>',
+            to: name + "," + email_id,
+            subject: "Verification Mail",
+            text: "",
+            html: `<a href="http://localhost:3002/user/verify/` + user_otp + `">Click Here to register in NodeJS API Server</a>`,
+        });
+        console.log("Message sent: %s", info.messageId);
+    },
+    verify_url: (req, res) => {
+        new_user.findAll({
+            where: {
+                otp: req.params.otp
+            },
+            raw: true
+        }).then((details) => {
+            console.log(details)
+            if (details.length != 0) {
+                var data = details.map((details) => {
+                    return {
+                        name: details.name,
+                        otp: details.otp,
+                        mail: details.mail
+                    };
+                })
+                res.json(data)
+            } else {
+                res.json({
+                    error: "Link Expired please re-register"
+                })
+            }
+        }).catch((error) => {
+            console.log(error)
+            res.json({
+                error: error
+            })
+        });
     },
     create_user: (req, res) => {
         controller.check_if_user_exists(req.body.mail)
@@ -120,11 +170,23 @@ let controller = {
                 if (!x) {
                     controller.check_if_user_already_registered(req.body.mail).then((user_registered) => {
                         if (!user_registered) {
-                            controller.create_unverified_new_user(req.body.mail, req.body.name)
+                            let user_otp = controller.generateOTP(15)
+                            controller.create_unverified_new_user(req.body.mail, req.body.name, user_otp)
                             console.log("New User , proceeding with E-Mail Verification")
+                            controller.send_verification_mail(req.body.mail, req.body.name, user_otp).then(() => {
+                                console.log("Mail Sent")
+                            }).catch((err) => {
+                                console.log(err)
+                            })
                         } else {
-                            controller.update_unverified_new_user(req.body.mail, req.body.name)
-                            console.log("Sending New Verification E-Mail")
+                            let user_otp = controller.generateOTP(15)
+                            controller.update_unverified_new_user(req.body.mail, req.body.name, user_otp)
+                            console.log("Resending Verification E-Mail")
+                            controller.send_verification_mail(req.body.mail, req.body.name, user_otp).then(() => {
+                                console.log("Mail Sent")
+                            }).catch((err) => {
+                                console.log(err)
+                            })
                         }
                     }).catch((e) => {
                         console.log(e)
