7b3a7bf94187b1003cf60f248752b30c83065245
Ruban Raj David <rajruban.d@gmail.com>
2020-07-25 16:04:16 +0530

    Added Handlebars Template Engine and Static folder

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const db = require("./src/model/sqlconnection");
+const hbs = require("express-handlebars");
+const path = require("path");
 
 //Create an object in Express
 const app = express();
+
+//Setup for Handlebars
+app.set("views", path.join(__dirname, "src/views"));
+app.engine(
+  "handlebars",
+  hbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "/src/views/layouts"),
+  }),
+);
+app.set("view engine", "handlebars");
 
 //Retiving data from req sent as JSON in body
 app.use(
@@ -16,22 +29,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
 //Routes
-app.use('/', require('./src/routes/general'))
-app.use('/user', require('./src/routes/user'))
+app.use("/", require("./src/routes/general"));
+app.use("/user", require("./src/routes/user"));
 
 //Test DB Connection
 db.authenticate()
   .then(() => {
-    console.log("connected");
+    console.log("Database Connected");
   })
-  .catch((error) => console.log(error, "Please Start MySQL Server"));
-
-//Assign the View folder as static address
-app.use(express.static("./view"));
+  .catch((error) =>
+    console.log("Please Start MySQL Server / Check Credentials"),
+  );
 
 //Log each request URI ,Status and its response time
 app.use(morgan("dev"));
 
+//Add Public as Static folder
+app.use(express.static("./src/views/public"));
 
 //Start Express Server
 app.listen(3002, () => {
