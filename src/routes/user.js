3bebcbac1c67000122ef77e096d91995ff0fdc10
Ruban Raj David <rajruban.d@gmail.com>
2020-07-28 21:39:43 +0530

    Added Routes for Forgot Password Module

--- src/routes/user.js
+++ src/routes/user.js
@@ -18,16 +18,28 @@ router.post("/create", controller.create_user)
 //Register User by getting all details
 router.get("/verify/:otp", controller.verify_url)
 
-//Registering user details
+//Registering user Controller
 router.post("/register", controller.register)
 
-//Register Page
+//Register Page HTML Send
 router.get("/create", controller.register_html)
 
-//register html send file
+//Login controller
 router.post("/login", controller.login)
 
-//Login Page
+//Login Page HTML send
 router.get("/login", controller.login_html)
+
+//Forgot Password Controller
+router.post("/reset-password", controller.forgot_pwd)
+
+//Forgot Password HTML send
+router.get("/reset-password", controller.forgot_pwd_html)
+
+//Forgot Password verification link
+router.get("/verify-reset/:otp", controller.forgot_pwd_mail_verify)
+
+//Forgot Password reset submit
+router.post("/verify-reset", controller.forgot_pwd_change)
 
 module.exports = router
