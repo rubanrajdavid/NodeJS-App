1d1a211ec09e2861fe5a49938eae53cbfd103712
Ruban Raj David <rajruban.d@gmail.com>
2020-08-12 22:14:38 +0530

    Started building Application View Structures

---
+++ src/views/public/assets/script/appLayout.js
@@ -0,0 +1,10 @@
+var header = document.getElementById("active-item");
+var btns = header.getElementsByClassName("nav-link");
+console.log(btns)
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
